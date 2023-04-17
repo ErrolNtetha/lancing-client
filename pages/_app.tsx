9b7f3823ca103fa34df824c7c2a7ae03d1e1959c
ErrolNtetha <mphumier@gmail.com>
2023-04-17 09:58:24 +0200

    feat: imported header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Header } from '../components/header'
+// import { Inter } from '@next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] })
+// className={inter.className}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main>
+            <Header />
+            <Component {...pageProps} />
+        </main>
+    )
 }
 
 export default MyApp
