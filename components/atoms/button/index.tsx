cc10d755f856882fc5375c806f148560c23fae92
ErrolNtetha <mphumier@gmail.com>
2023-04-17 14:48:20 +0200

    refactor: added type prop and button text prop

--- components/atoms/button/index.tsx
+++ components/atoms/button/index.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 interface ButtonProps {
     handleClick: React.MouseEventHandler<HTMLButtonElement>;
     className: string;
+    buttonText: string;
 }
 
-export const Button = ({ handleClick, className }: ButtonProps) => (
-    <button type='submit' className={className} onClick={handleClick}> Subscribe Now </button>
+export const Button = ({ handleClick, className, buttonText }: ButtonProps) => (
+    <button type='submit' className={className} onClick={handleClick}> {buttonText} </button>
 );
