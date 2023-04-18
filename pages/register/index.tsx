7520c56a55d90eb4870bd61b7a706dc0b56570d6
ErrolNtetha <mphumier@gmail.com>
2023-04-18 11:05:26 +0200

    feat: page for displaying registration form

---
+++ pages/register/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RegisterForm } from '../../components/organisms/register';
+
+const Register = () => {
+    return (
+        <section className='bg-slate'>
+            <RegisterForm />
+        </section>
+    );
+};
+
+export default Register;
