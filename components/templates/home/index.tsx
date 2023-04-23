f3e49bb823501aec63c8d32d096ad0b3a87f7180
ErrolNtetha <mphumier@gmail.com>
2023-04-23 07:46:59 +0200

    feat: home

---
+++ components/templates/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ClientProject } from '../../organisms/project';
+
+export const Home = () => {
+    return (
+        <section className=''>
+            Client component
+            <ClientProject />
+        </section>
+    );
+};
