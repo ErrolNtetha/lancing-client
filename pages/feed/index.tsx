a7a207aaffd71a7d02b0c98bef8a7938ca6dc687
ErrolNtetha <mphumier@gmail.com>
2023-04-23 07:47:11 +0200

    feat: feed page

---
+++ pages/feed/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Client } from '../../components/templates/feed';
+
+const Feed = () => {
+    return (
+        <section className='bg-white-p h-[95vh]'>
+            <Client />
+        </section>
+    );
+};
+
+export default Feed;
