0bb80e9e633e1535eec166d05a053d70bb8a8931
ErrolNtetha <mphumier@gmail.com>
2023-04-25 09:34:22 +0200

    feat: nav links

---
+++ components/molecules/nav/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+
+export const Nav = () => {
+    return (
+        <nav className='md:flex items-center space-x-8 gap-8'>
+            <ul className='md:flex text-black md:text-black'>
+                <li className='text-md cursor-pointer font-semibold pl-4 py-2'> 
+                    <Link href='#our-vision'> Our Vision </Link>
+                </li>
+                <li className='text-sm cursor-pointer font-semibold pl-4 py-2'>
+                    <Link href='#about'> About </Link>
+                </li>
+                <li className='text-sm cursor-pointer font-semibold pl-4 py-2'>
+                    <Link href='#contact'> Contact </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
