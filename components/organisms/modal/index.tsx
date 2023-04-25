dfe5046d3a4d9dd179b7422544daff4c078fe51d
ErrolNtetha <mphumier@gmail.com>
2023-04-25 10:05:46 +0200

    feat: minor changes

--- components/organisms/modal/index.tsx
+++ components/organisms/modal/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Modal = ({ children }: Props) => {
     return (
         <section className='text-sm fixed top-0 left-0 bottom-0 w-full flex justify-center items-center bg-white-o backdrop-blur-md'>
-            <section className='bg-white-p shadow-md border border-gray max-h-[80%] w-[90%] overflow-y-auto md:w-[30%] relative'> 
+            <section className='bg-white-p shadow-md border border-gray max-h-[80%] w-[90%] overflow-y-auto md:w-[50%] lg:w-[30%] relative'> 
                 {children}
             </section>
         </section>
