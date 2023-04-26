9d3e7bc6c4ca8c81b4b69bcb910e76a2ce949186
ErrolNtetha <mphumier@gmail.com>
2023-04-26 20:46:11 +0200

    feat: added a sample profile

--- hooks/useGlobalStore.ts
+++ hooks/useGlobalStore.ts
@@ -1,13 +1,18 @@
 import { create } from 'zustand';
 
-export const useUserStore = create((set) => ({
-    users: [],
-    addUser: () => set((state) => ({ count: state.count + 1 })),
-    removeUser: () => set((state) => ({ count: state.count - 1 })),
-}));
+type Profile = {
+    firstName: string;
+    lastName: string;
+    role: 'client' | 'vendor';
+};
+
+const myProfile: Profile = {
+        firstName: 'Mphumeleli Errol',
+        lastName: 'Ntetha',
+        role: 'client'
+    }
 
 export const useProfileStore = create((set) => ({
-    profile: [],
-    addProfile: (user) => set(() => ({ profile: user })),
-    removeProfile: () => set({}, true),
+    profile: {...myProfile},
+    switchProfile: (user: Profile) => set(() => ({ profile: user })),
 }));
