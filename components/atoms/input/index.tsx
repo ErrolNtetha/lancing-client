0b82375ff2f2b730425ad81e82b5baf6b39d8b52
ErrolNtetha <mphumier@gmail.com>
2023-04-26 09:31:52 +0200

    feat: added props for register function

--- components/atoms/input/index.tsx
+++ components/atoms/input/index.tsx
@@ -1,35 +1,34 @@
 import React from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
 const inputContainer = 'flex items-center justify-between p-2 bg-white-p border border-gray outline-black w-full block';
 
 export interface InputProps {
-    value: string;
     placeholder: string;
     name: string;
     type: string;
     hasHideIcon?: boolean;
     isHidden?: boolean;
-    onChange: React.ChangeEventHandler<HTMLInputElement>
     handleHideIcon?: React.MouseEventHandler<HTMLSpanElement>;
+    required: boolean;
+    register: Function;
 }
 
-export const Input = ({ value, name, type, onChange, handleHideIcon, placeholder, hasHideIcon = false, isHidden }: InputProps) => {
+export const Input = ({ name, type, required, register, handleHideIcon, placeholder, hasHideIcon = false, isHidden }: InputProps) => {
     return (
         <section className={inputContainer}>
             <input
                 type={type}
-                name={name}
                 placeholder={placeholder}
                 autoComplete='off'
                 className='outline-none w-full'
-                value={value}
-                onChange={onChange}
+                {...register(name, { required })}
             />
-            { (hasHideIcon && value.length > 0) && 
-            <span onClick={handleHideIcon} className='pl-4 hover:cursor-pointer'> 
-                { isHidden ? 'Show' : 'Hide' }
-            </span> 
-            }
+            {hasHideIcon && (
+                <span onClick={handleHideIcon} className='pl-4 hover:cursor-pointer'> 
+                    { isHidden ? <FiEye /> : <FiEyeOff /> }
+                </span> 
+            )}
         </section>
     );
 };
