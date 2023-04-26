1a6178bc843de9c5783c499589252d7abb6d06c1
ErrolNtetha <mphumier@gmail.com>
2023-04-26 09:30:01 +0200

    feat: passed prop and error message

--- components/molecules/formLabel/index.tsx
+++ components/molecules/formLabel/index.tsx
@@ -3,33 +3,42 @@ import { Input, InputProps } from '../../atoms/input';
 import { Label } from '../../atoms/label';
 
 interface IProps extends InputProps {
-    onChange: React.ChangeEventHandler<HTMLInputElement>;
-    value: string;
     placeholder: string;
-    htmlFor: string;
     labelName: string;
-    name: string;
     type: string;
     handleHideIcon?: React.MouseEventHandler<HTMLSpanElement>;
     hasHideIcon?: boolean;
     isHidden?: boolean;
+    errors?: string | undefined;
 }
 
-export const FormLabel = ({ onChange, handleHideIcon, isHidden, type, value, name, placeholder, htmlFor, hasHideIcon, labelName  }: IProps) => {
+export const FormLabel = ({ 
+    name, 
+    required, 
+    register, 
+    handleHideIcon, 
+    isHidden, 
+    type, 
+    placeholder, 
+    hasHideIcon, 
+    labelName,
+    errors
+}: IProps) => {
     return (
         <section className='py-2'>
-            <Label htmlFor={htmlFor} labelName={labelName}
+            <Label htmlFor={labelName} labelName={labelName}
             />
             <Input
                 type={type}
-                value={value}
                 handleHideIcon={handleHideIcon}
-                name={name}
                 hasHideIcon={hasHideIcon}
-                onChange={onChange}
                 placeholder={placeholder}
                 isHidden={isHidden}
+                name={name}
+                register={register}
+                required={required}
             />
+            {errors === 'required' && <p className='text-xs py-1 text-[red]'> This field is required. </p>}
         </section>
     );
 };
