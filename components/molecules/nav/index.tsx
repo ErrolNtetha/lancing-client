ca07139d210a6a608aac89671f53194c527e8750
ErrolNtetha <mphumier@gmail.com>
2023-04-29 23:20:08 +0200

    style: removed padding left

--- components/molecules/nav/index.tsx
+++ components/molecules/nav/index.tsx
@@ -5,13 +5,13 @@ export const Nav = () => {
     return (
         <nav className='md:flex items-center space-x-8 gap-8'>
             <ul className='md:flex text-white md:text-black'>
-                <li className='text-md cursor-pointer font-semibold pl-4 py-2'> 
+                <li className='text-md cursor-pointer py-2'> 
                     <Link href='#our-vision'> Our Vision </Link>
                 </li>
-                <li className='text-sm cursor-pointer font-semibold pl-4 py-2'>
+                <li className='text-md cursor-pointer py-2'>
                     <Link href='#about'> About </Link>
                 </li>
-                <li className='text-sm cursor-pointer font-semibold pl-4 py-2'>
+                <li className='text-md cursor-pointer py-2'>
                     <Link href='#contact'> Contact </Link>
                 </li>
             </ul>
