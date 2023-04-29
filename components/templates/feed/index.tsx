6685cd6be74e085f8c0bf7f73696a8e4953712e6
ErrolNtetha <mphumier@gmail.com>
2023-04-29 11:55:27 +0200

    feat: templates for the feed

--- components/templates/feed/index.tsx
+++ components/templates/feed/index.tsx
@@ -2,10 +2,11 @@ import Head from 'next/head';
 import React from 'react';
 import { useProfileStore } from '../../../hooks/useGlobalStore';
 import { Client } from '../../organisms/client/';
+import { Vendor } from '../../organisms/vendor';
 
 export const ClientUI = () => {
     const isClient = useProfileStore((state) => state.profile?.isClient);
-    const renderUI = isClient ? 'Vendor UI' : <Client />;
+    const renderUI = isClient ? <Vendor /> : <Client />;
 
     return (
         <>
