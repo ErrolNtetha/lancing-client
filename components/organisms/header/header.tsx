5a1b75fdd522c78ecfa8ec7f905db9f84176705f
ErrolNtetha <mphumier@gmail.com>
2023-04-29 23:19:35 +0200

    feat: add profile section

--- components/organisms/header/header.tsx
+++ components/organisms/header/header.tsx
@@ -4,11 +4,14 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 // import { useScreenWidth } from '../../../hooks/useScreenWidth';
 import { FiAlignRight } from 'react-icons/fi';
+import { useProfileStore } from '../../../hooks/useGlobalStore';
 import { Button } from '../../atoms/button';
 import { MobileMenu } from './mobileMenu';
 
 export const Header = () => {
     const [nav, setNav] = useState(false);
+    const p = useProfileStore();
+    const { profile } = p;
 
     const handleLogout = () => console.log('logged out');
 
@@ -17,11 +20,11 @@ export const Header = () => {
             <Image src='/images/logo.svg' alt='company logo' className='' width={80} height={20} />
             <FiAlignRight onClick={() => setNav(!nav)} className='hover:cursor-pointer text-[1.8rem]' />
             {nav && (
-                <section className='fixed backdrop-blur-md top-0 left-0 bottom-0 w-full'>
-                    <MobileMenu handleMenuToggle={() => setNav(!nav)} />
-                    <section className='flex-col justify-center absolute w-full left-0 bottom-2'>
+                <section className='fixed bg-slate top-0 left-0 bottom-0 w-full'>
+                    <MobileMenu avatar={profile.avatar} name={profile.name} handleMenuToggle={() => setNav(!nav)} />
+                    <section className='flex items-center justify-center absolute w-full left-0 bottom-4'>
                         <Button
-                            className='bg-slate font-extrabold w-full p-2 my-1 mx-4'
+                            className='border-2 border-white font-extrabold w-full p-2 my-1 mx-4'
                             buttonText='Logout'
                             handleClick={handleLogout}
                         />
