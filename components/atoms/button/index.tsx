d7aa9a85e284dad75d3bcd02974018e122a3487a
ErrolNtetha <mphumier@gmail.com>
2023-05-12 09:38:44 +0200

    feat: add type

--- components/atoms/button/index.tsx
+++ components/atoms/button/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 interface ButtonProps {
-    handleClick: React.MouseEventHandler<HTMLButtonElement>;
+    type?: 'submit' | 'button'
+    handleClick?: React.MouseEventHandler<HTMLButtonElement>;
     className: string;
     buttonText: string | React.ReactNode;
 }
 
-export const Button = ({ handleClick, className, buttonText }: ButtonProps) => (
-    <button type='submit' className={className} onClick={handleClick}> {buttonText} </button>
+export const Button = ({ handleClick, className, type = 'submit', buttonText }: ButtonProps) => (
+    <button type={type} className={className} onClick={handleClick}> {buttonText} </button>
 );
