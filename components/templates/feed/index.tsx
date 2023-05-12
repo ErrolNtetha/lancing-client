615acfa9a8c55e84cf13c4b702ae45f60fffd890
ErrolNtetha <mphumier@gmail.com>
2023-05-12 09:39:18 +0200

    feat: added a post button

--- components/templates/feed/index.tsx
+++ components/templates/feed/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
 import { useProfileStore } from '../../../hooks/useGlobalStore';
 import { Client } from '../../organisms/client/';
+import { PostGig } from '../../organisms/client/post';
+import { CreatePost } from '../../organisms/createPost';
+import { Modal } from '../../organisms/modal';
 import { Vendor } from '../../organisms/vendor';
 
 export const ClientUI = () => {
+    const [modal, setModal] = useState(false);
     const isClient = useProfileStore((state) => state.profile?.isClient);
     const renderUI = isClient ? <Vendor /> : <Client />;
 
@@ -14,6 +18,13 @@ export const ClientUI = () => {
                 <title> Feed | Duello </title>
             </Head>
 
+            {isClient && <CreatePost handlePost={() => setModal(!modal)} />}
+            {modal && 
+                <Modal>
+                    <PostGig 
+                        handleModalToggle={() => setModal(!modal)}
+                    />
+                </Modal>}
             {renderUI}
         </>
     );
