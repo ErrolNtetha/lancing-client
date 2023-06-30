5c2df316bff345f06c3512c774e2587e7f46e06c
ErrolNtetha <mphumier@gmail.com>
2023-06-30 09:37:51 +0200

    refactor: header now responsive

--- components/organisms/header/header.tsx
+++ components/organisms/header/header.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 // import { useScreenWidth } from '../../../hooks/useScreenWidth';
 import { FiAlignRight } from 'react-icons/fi';
@@ -13,8 +14,13 @@ export const Header = () => {
     const [nav, setNav] = useState(false);
     const p = useProfileStore();
     const { profile } = p;
+    const router = useRouter();
 
-    const handleLogout = () => console.log('logged out');
+    const handleLogout = () => {
+        console.log('logged out')
+        setNav(!nav);
+        router.push('/login');
+    };
 
     return (
         <header className='flex z-10 justify-between items-center relative text-white bg-slate h-[5vh] px-6'>
@@ -22,7 +28,7 @@ export const Header = () => {
             <FiAlignRight onClick={() => setNav(!nav)} className='hover:cursor-pointer text-[1.8rem] block md:hidden translate-y-[-50%] absolute top-[50%] right-[25px] transition-transform' />
             <section className='ml-auto'>
                 <ul className='p-0 hidden md:flex'>
-                    <li className='ml-4 mr-4'>
+                    <li className='ml-4 mr-4' >
                         <Link className='block w-full' href='/feed'>Home</Link>
                     </li>
                     <li className='ml-4 mr-4'>
