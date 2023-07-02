dfdab758805b89c257791db26ddc2ee41caf3e7b
ErrolNtetha <mphumier@gmail.com>
2023-07-02 11:36:25 +0200

    refactor: moved type to another file and declare typings for react stars package

--- typings.d.ts
+++ typings.d.ts
@@ -1,11 +1,1 @@
-export interface InputProps {
-    placeholder: string;
-    name: string;
-    type?: string;
-    hasHideIcon?: boolean;
-    isHidden?: boolean;
-    handleHideIcon?: React.MouseEventHandler<HTMLSpanElement>;
-    required: boolean;
-    register: Function;
-    disabled?: boolean;
-}
+declare module 'react-rating-stars-component';
