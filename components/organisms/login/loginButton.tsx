4d5ff384fdde8ca2878c22ca13b9bf7dee519a78
ErrolNtetha <mphumier@gmail.com>
2023-07-08 19:41:12 +0200

    feat: added a left margin

--- components/organisms/login/loginButton.tsx
+++ components/organisms/login/loginButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const LoginButton = () => {
     return (
-        <Link href='/login' className='hidden md:block border-2 py-1 px-5 hover:cursor-pointer hover:bg-gray'>
+        <Link href='/login' className='hidden md:block ml-3 border-2 py-1 px-5 hover:cursor-pointer hover:bg-gray'>
             Login
         </Link>
     );
