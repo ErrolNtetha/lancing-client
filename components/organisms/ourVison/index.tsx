e5301a42bd1501e8560a59c7ef740361a6e3e769
ErrolNtetha <mphumier@gmail.com>
2023-07-17 15:26:06 +0200

    style: increased text size

--- components/organisms/ourVison/index.tsx
+++ components/organisms/ourVison/index.tsx
@@ -6,11 +6,11 @@ export const OurVision = () => {
         <section className='flex items-center justify-center bg-vision-bg bg-cover h-screen'>
             <section className='md:flex items-center justify-center'>
                 <aside className='text-black max-w-30 m-8'>
-                    <h4 className='text-sm md:text-lg font-bold opacity-30'> Our Vision </h4>
+                    <h4 className='text-md md:text-lg font-bold opacity-30'> Our Vision </h4>
                     <h1 className='max-w-md font-extrabold text-black text-2xl mb-2'>
                         Striving to be the go-to destination for anyone seeking quality freelance services.
                     </h1>
-                    <p className='max-w-lg text-sm mb-8 md:text-lg'> 
+                    <p className='max-w-lg text-md mb-8 md:text-lg'> 
                         At Duello, our vision is to empower freelancers in South Africa to work smarter, not harder.
                         We believe that by simplifying the process of finding and hiring top talent, we can enable individuals to 
                         achieve their goals faster and more efficiently.
