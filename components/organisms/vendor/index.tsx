ee59de332d8f066efbfc037136506c5ed944e2b8
ErrolNtetha <mphumier@gmail.com>
2023-07-26 07:19:51 +0200

    feat: added a section element

--- components/organisms/vendor/index.tsx
+++ components/organisms/vendor/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { ListVendors } from './listVendors';
 
-export const Vendor = () => <ListVendors />
+export const Vendor = () => <section> <ListVendors /> </section>;
