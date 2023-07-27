319e8802127a94f4c15275d72a1a41ad76f4e064
ErrolNtetha <mphumier@gmail.com>
2023-07-27 08:42:07 +0200

    feat: new users on a seperate file

---
+++ components/organisms/vendor/vendors.ts
@@ -0,0 +1,68 @@
+export const vendors = [
+    {
+        recipient: {
+            firstName: 'Snenhlanhla',
+            lastName: 'Radebe',
+        },
+        service: 'Copywriter',
+        amount: 5800,
+        rating: 4,
+        pitchText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati fugiat est hic. Voluptatem quam consequatur hic harum qui earum ipsa omnis quaerat animi. Ipsam facere impedit laborum delectus nostrum magnam!',
+        avatar: '/images/users/woman.jpg',
+        banned: {
+            isBanned: false,
+            startDate: null,
+            endDate: null,
+            reason: null,
+        },
+        reviews: [
+            {
+                firstName: 'Mphumeleli',
+                lastName: 'Ntetha',
+                comment: 'I am happy with the work i was provided. I really recommend Snenhlanhla!',
+            },
+        ],
+    },
+    {
+        recipient: {
+            firstName: 'Sandile',
+            lastName: 'Dlamini',
+        },
+        service: 'Logo Designer',
+        amount: 4200,
+        rating: 5,
+        pitchText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati fugiat est hic. Voluptatem quam consequatur hic harum qui earum ipsa omnis quaerat animi. Ipsam facere impedit laborum delectus nostrum magnam!',
+        avatar: '/images/users/man.jpg',
+        banned: {
+            isBanned: false,
+            startDate: null,
+            endDate: null,
+            reason: null,
+        },
+        reviews: [],
+    },
+    {
+        recipient: {
+            firstName: 'John',
+            lastName: 'Smith',
+        },
+        service: 'Video Editor',
+        amount: 6500,
+        rating: 3,
+        pitchText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati fugiat est hic. Voluptatem quam consequatur hic harum qui earum ipsa omnis quaerat animi. Ipsam facere impedit laborum delectus nostrum magnam!',
+        avatar: '/images/users/guy.jpg',
+        banned: {
+            isBanned: false,
+            startDate: null,
+            endDate: null,
+            reason: null,
+        },
+        reviews: [
+            {
+                firstName: 'Mphumeleli',
+                lastName: 'Ntetha',
+                comment: 'I am happy with the work i was provided. I really recommend Snenhlanhla!',
+            },
+        ],
+    }
+];
