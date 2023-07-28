304a2a8c3a439ffaca3115e575f2abca4dc5efad
ErrolNtetha <mphumier@gmail.com>
2023-07-28 18:15:08 +0200

    refactor: changed the prop name forom recipient to names

--- components/organisms/vendor/vendorComponent.tsx
+++ components/organisms/vendor/vendorComponent.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { FiSend, FiUser } from 'react-icons/fi';
+import { VendorProps } from '../../../types';
 import { formatAmount } from '../../../utilities/format';
 import { Avatar } from '../../molecules/image';
 import { StarRating } from '../../molecules/star-rating';
@@ -8,37 +9,21 @@ import { Modal } from '../modal';
 import { EnquiryModal } from './enquiryModal';
 import { PortfolioModal } from './viewPortfolio/';
 
-type VendorProps = {
-    recipient: {
-        firstName: string;
-        lastName: string;
-    },
-    avatar: string;
-    service: string;
-    rating: number;
-    pitchText: string;
-    amount: number;
-    reviews: {
-        firstName: string;
-        lastName: string;
-        comment: string;
-    }[];
-};
-
 export const VendorComponent = ({
     avatar,
-    recipient,
+    names,
     service,
     rating,
-    pitchText,
+    description,
     amount,
-    reviews
+    reviews,
 }: VendorProps) => {
     const [modal, setModal] = useState(false);
     const [viewPortfolio, setViewPortfolio] = useState(false);
 
     const handleModal = () => setModal(!modal);
-    const clientReviews = !reviews.length ? 'No reviews' : `${reviews.length} reviews`;
+    const isPlural = reviews.length > 1 ? 'reviews' : 'review';
+    const clientReviews = !reviews.length ? 'No reviews' : `${reviews.length} ${isPlural}`;
 
     return (
         <section className='text-black text-[.8rem] md:text-sm my-3 border border-gray bg-white shadow-md w-full md:w-[35rem] max-h-max p-2'>
@@ -47,23 +32,23 @@ export const VendorComponent = ({
                     <span className='flex gap-2'>
                         <Avatar
                             src={avatar}
-                            alt={`${recipient.firstName}&apos;s avatar`}
+                            alt={`${names.firstName}&apos;s avatar`}
                             size='w-14 h-14'
                         />
                         <span>
-                            <h2 className='text-md md:text-lg font-semibold'>{recipient.firstName} {recipient.lastName} </h2>
+                            <h2 className='text-md md:text-lg font-semibold'>{names.firstName} {names.lastName} </h2>
                             <p className='flex items-center gap-1'> {service} - <Link className='text-[blue]' href='/postId/reviews'> {clientReviews} </Link> </p>
                             <span className='flex items-center gap-1'> <StarRating value={rating} /> ({rating}/5) </span>
                         </span>
                     </span>
                     { /* <span className='self-start'> <FiMoreHorizontal /> </span> */ }
                 </section>
-                <p className='mb-4'> {pitchText} </p>
+                <p className='mb-4'> {description} </p>
                 <hr className='opacity-10 mb-2' />
                 <section className='flex justify-between items-center text-sm'>
                     <span className='flex items-center gap-2'>
                         <button className='flex items-center gap-1 hover:cursor-pointer'onClick={() => setViewPortfolio(!viewPortfolio)}> <FiUser /> View </button>|
-                        <span className='py-2 text-[.7rem]'> From: R{formatAmount(amount)} </span>
+                        <span className='py-2 text-[.7rem]'> From: R{formatAmount(amount)} {/* { percent && ` | ${percent}%`} */} </span>
                     </span>
 
                     <button className='flex items-center gap-2 border-2 border-gray px-2 py-1 hover:cursor-pointer hover:bg-gray' onClick={handleModal}>
@@ -72,7 +57,7 @@ export const VendorComponent = ({
                     {viewPortfolio && (
                         <Modal>
                             <PortfolioModal 
-                                name={recipient}
+                                name={names}
                                 title={service}
                                 handleModal={() => setViewPortfolio(!viewPortfolio)}
                             />
@@ -82,7 +67,7 @@ export const VendorComponent = ({
                         <Modal>
                             <EnquiryModal
                                 handleModal={handleModal}
-                                recipient={recipient}
+                                recipient={names}
                             />
                         </Modal> 
                     }
