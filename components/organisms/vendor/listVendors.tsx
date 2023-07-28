4516ce00964497428ddb81e3b8661231aad2033b
ErrolNtetha <mphumier@gmail.com>
2023-07-28 18:17:03 +0200

    feat: minor changes

--- components/organisms/vendor/listVendors.tsx
+++ components/organisms/vendor/listVendors.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/jsx-key */
 
-import React from 'react';
-import { VendorComponent } from './vendorComponent';
-import { v4 as uuidv4 } from 'uuid';
-import { vendors } from './vendors';
+import React from "react";
+import { VendorComponent } from "./vendorComponent";
+import { v4 as uuidv4 } from "uuid";
+import { vendors } from "./vendors";
 
 export const ListVendors = () => {
-    return (
-        <React.Fragment>
-            {vendors.map((vendor) => <VendorComponent key={uuidv4()} {...vendor} />)}
-        </React.Fragment>
-    );
+	return (
+		<React.Fragment>
+			{vendors.map((vendor) => (
+				<VendorComponent key={uuidv4()} {...vendor} />
+			))}
+		</React.Fragment>
+	);
 };
