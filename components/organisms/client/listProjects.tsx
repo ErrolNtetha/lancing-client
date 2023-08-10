17284ce3a14835131a7f8f8ee35f638fc6f4ec1f
ErrolNtetha <mphumier@gmail.com>
2023-08-10 15:11:11 +0200

    feat: added a prompt

--- components/organisms/client/listProjects.tsx
+++ components/organisms/client/listProjects.tsx
@@ -3,6 +3,7 @@
 import Head from 'next/head';
 import React from 'react';
 import { generateRanomId } from '../../../utilities/generateRandomId';
+import Prompt from '../vendor/prompt';
 import { ClientProject } from './clientProject';
 import { clients } from './clients';
 
@@ -12,6 +13,7 @@ export const ListProjects = () => {
             <Head>
                 <title> Browse list of projects - Duello </title>
             </Head>
+            <Prompt />
             {clients.map((client) => <ClientProject key={generateRanomId()} {...client} /> )}
         </section>
     );
