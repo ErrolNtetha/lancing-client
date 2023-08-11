a44f58584dcc0360f2d2b6cd1b1a7e45f63facf5
ErrolNtetha <mphumier@gmail.com>
2023-08-11 15:18:14 +0200

    feat: get projects from database

--- components/organisms/client/listProjects.tsx
+++ components/organisms/client/listProjects.tsx
@@ -1,20 +1,46 @@
 /* eslint-disable react/jsx-key */
 
-import Head from 'next/head';
+import { collection, getDocs } from 'firebase/firestore';
+// import Head from 'next/head';
 import React from 'react';
+import { db } from '../../../firebaseConfig';
 import { generateRanomId } from '../../../utilities/generateRandomId';
-import Prompt from '../vendor/prompt';
+import { NoContent } from '../../molecules/noContent';
+// import Prompt from '../vendor/prompt';
 import { ClientProject } from './clientProject';
-import { clients } from './clients';
+// import { clients } from './clients';
 
 export const ListProjects = () => {
+    async function getProjects() {
+        try {
+            const projectsRef = collection(db, 'projects');
+            const p = await getDocs(projectsRef);
+            p.forEach((doc) => {
+                if (!doc) {
+                    console.log('no projects');
+                    return;
+                }
+                console.log('Projects data: ', doc.data());
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const clientsProjects = getProjects();
+
+    //@ts-ignore
+    const projects = !clientsProjects?.length
+        ? <NoContent 
+            main='No projects yet.'
+            body=' Projects posted by clients will appear here. Check back later.'
+        />
+        // @ts-ignore
+        : clientsProjects.map((project: any) => <ClientProject key={generateRanomId()} {...project} />);
+
     return (
-        <section>
-            <Head>
-                <title> Browse list of projects - Duello </title>
-            </Head>
-            <Prompt />
-            {clients.map((client) => <ClientProject key={generateRanomId()} {...client} /> )}
+        <section className='flex-1 h-[92vh]'>
+            {projects}
         </section>
     );
 };
