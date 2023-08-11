371300cdd4798350d206a00fc943e65b5eeaa2c6
ErrolNtetha <mphumier@gmail.com>
2023-08-11 18:45:13 +0200

    feat: nav item for messages added

--- components/molecules/sidebar/index.tsx
+++ components/molecules/sidebar/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FiMessageSquare } from 'react-icons/fi';
 import { useProfileStore } from '../../../hooks/useGlobalStore';
 import { PostGig } from '../../organisms/client/post';
 import { Modal } from '../../organisms/modal';
+import { DigitCounter } from '../digitCounter';
 
 export const Sidebar = () => {
     const [modal, setIsModal] = React.useState(false);
@@ -9,7 +11,11 @@ export const Sidebar = () => {
 
     return (
         <aside className='hidden p-2 max-h-max flex-[.4] md:block'>
-            <ul>
+            <ul className='divide-gray'>
+                <li className='flex items-center justify-between divide-gray hover:cursor-pointer hover:bg-gray p-2'>
+                    <span className='flex items-center gap-3'> <FiMessageSquare /> Messages </span>
+                    <DigitCounter count={4} />
+                </li>
                 {isClient && (
                     <li className='bg-slate text-black p-2 hover:cursor-pointer'>
                         <button className='text-white text-center w-full' onClick={() => setIsModal(!modal)}> Post </button>
