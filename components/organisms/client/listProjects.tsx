6e91d3c5e93a6c4f85c12e5d2f29f10bbe0ee0b3
ErrolNtetha <mphumier@gmail.com>
2023-08-21 13:45:24 +0200

    feat: render projects on the page

--- components/organisms/client/listProjects.tsx
+++ components/organisms/client/listProjects.tsx
@@ -2,27 +2,40 @@
 
 import React from 'react';
 import { useFetchProjects } from '../../../hooks/useFetchProjects';
-// import { generateRanomId } from '../../../utilities/generateRandomId';
 import { ClientProject } from './clientProject';
 
 export const ListProjects = () => {
-    const { docs, loading } = useFetchProjects();
-
-    const projects = !docs.length
-        ? (
-            <section className='flex justify-center items-center h-full'> 
-                <span className='text-center w-[80%]'>
-                    <p className='font-bold'> No projects yet. </p>
-                    <p className='py-3 text-sm md:text-md'> Projects posted by clients will appear here. Check back later. </p>
-                </span>
-            </section>)
-            //@ts-ignore
-        : docs.map((doc) => <ClientProject loading={loading} projectId={doc.id} key={doc.id} {...doc.project} />)
+    const { docs } = useFetchProjects();
 
     return (
         <React.Fragment>
             <section className='h-[92vh]'>
-                {projects}
+                {
+                    !docs.length 
+                    ? (
+                        <section className='flex justify-center items-center h-full'> 
+                            <span className='text-center w-[80%]'>
+                                <p className='font-bold'> No projects yet. </p>
+                                <p className='py-3 text-sm md:text-md'> 
+                                    Projects posted by clients will appear here. Check back later. 
+                                </p>
+                            </span>
+                        </section>)
+                    //@ts-ignore
+                    : docs.map((doc: any) => {
+                        const { id, project } = doc;
+                        if (project.postedBy) {
+                            return (
+                                <ClientProject
+                                    key={id}
+                                    projectId={id}
+                                    {...project.postedBy}
+                                    {...project}
+                                />
+                            )
+                        }
+                    })
+                }
             </section>
         </React.Fragment>
     );
