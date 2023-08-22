9634e25efc74b27db847ac778723b7af5c22e94b
ErrolNtetha <mphumier@gmail.com>
2023-08-22 19:40:11 +0200

    feat: rewrote founder statement

--- components/organisms/ourTeam/index.tsx
+++ components/organisms/ourTeam/index.tsx
@@ -11,21 +11,20 @@ export const OurTeam = () => {
                     alt='Smiling picture of the creator of Duello'
                 />
                 <section className='flex justify-center items-center mt-4 flex-col'>
-                    <h1 className='font-extrabold text-xl'> Mphumeleli Errol Ntetha </h1>
+                    <h1 className='font-extrabold text-xl'> Mphumeleli Ntetha </h1>
                     <p className='text-md'> Creator of Duello </p>
                 </section>
             </section>
             <span className='flex gap-y-6'>
-                <p className='m-6 text-white text-md md:text-lg max-w-md'> 
-                    <span className='text-white font-extrabold text-lg'>&lsquo;&lsquo;</span>
-                     I have been falling short of people who will do my
-                    tedious tasks that I have no idea about, and not knowing where to find them. That is why I have
-                    had to develop this platform.
-                    <span className='text-white font-extrabold text-lg'>&rsquo;&rsquo;</span>
+                <p className='m-6 text-white text-md md:text-md max-w-md'> 
+                    As a passionate advocate for freelancers and businesses alike, I embarked on a journey to redefine the freelancing 
+                    experience in South Africa. Duello isn&apos;t just a platform; it&apos;s a manifestation of my commitment to empowering talent, fostering 
+                    collaboration, and propelling projects to new heights.
                     <br />
                     <br />
-                    Born in Durban, South Africa, Mphumeleli is the creator of Duello and believes that we as South Africans, and Africa as a whole, have 
-                    more than enough capabilities to establish our very own products/services to showcase our skills. 
+                    With a deep-rooted belief in the power of connections, I envisioned a platform that brings together freelancers and clients in an environment 
+                    driven by innovation, integrity, and excellence. Our mission at Duello is to simplify the complex, to transform the mundane into the extraodinary, 
+                    and to unlock opportunities that transcend boundaries.
                 </p>
             </span>
         </section>
