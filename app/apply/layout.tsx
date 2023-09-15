117ea4217ccc032e95645b4c6fe4555e0bbce0e5
ErrolNtetha <mphumier@gmail.com>
2023-09-15 17:08:02 +0200

    feat: added a header in apply route

---
+++ app/apply/layout.tsx
@@ -0,0 +1,24 @@
+// import { FiUser } from 'react-icons/fi';
+import '../../styles/globals.css';
+import logo from '../../public/assets/images/svg/blackLogo.svg';
+import Image from 'next/image';
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html>
+            <head></head>
+            <body>
+                <header className='container h-[5vh] p-2'> 
+                    <section className='flex items-center justify-between'>
+                        <Image width={90} height={50} alt='Company logo' src={logo} />
+                    </section>
+                </header>
+                {children}
+            </body>
+        </html>
+    )
+}
