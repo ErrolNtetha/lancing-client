7bee9467952b176e304d5c5f06009819082d3333
ErrolNtetha <mphumier@gmail.com>
2023-09-16 18:11:15 +0200

    feat: protected route

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -1,31 +1,25 @@
 'use client'
 
-import { useRouter } from 'next/navigation';
 import React from 'react';
-import { useAuth } from '../../hooks/useAuth';
+import { AuthorizedRoute } from '../../components/organisms/protectedRoutes/authorizedRoute';
+// import { useAuth } from '../../hooks/useAuth';
 import { CreateApplication } from './createApplication';
 
-/*
- * Only allow vendors who are loggedin but not approved::: { isClient: false, isApproved: false }
- *
- */
-
 const Apply = () => {
-    const { loading, currentUser } = useAuth();
-    const router = useRouter();
-    
-    if (loading) {
-        return <section> loading... </section>
-    } else if (!currentUser) {
-        return router.push('/login');
-    }
+    // const { loading, currentUser } = useAuth();
 
     return (
-        <section className='h-[92vh]'>
-            <section className='container flex justify-center px-3 md:p-0 gap-4 '>
-                <CreateApplication />
+        <AuthorizedRoute 
+            isApproved={false}
+            isClient={false}
+            authenticated={true}
+        >
+            <section className='h-[92vh]'>
+                <section className='container flex justify-center px-3 md:p-0 gap-4 '>
+                    <CreateApplication />
+                </section>
             </section>
-        </section>
+        </AuthorizedRoute>
     );
 };
 
---
+++ components/organisms/protectedRoutes/authorizedRoute.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+type ARProps = {
+    isApproved?: boolean | null;
+    isClient?: boolean | null;
+    authenticated?: boolean | null;
+    children: React.ReactNode;
+}
+
+export const AuthorizedRoute = (props: ARProps) => {
+    const { children, isApproved, authenticated, isClient } = props;
+    const router = useRouter();
+    const Component = (authenticated && !isApproved && !isClient) ? children : router.push('login');
+
+    return (
+        <>
+            {Component}
+        </>
+    );
+};
