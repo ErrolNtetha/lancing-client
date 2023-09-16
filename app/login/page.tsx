56388170f81d86c2f15abea2fe60c7eb9f1bd180
ErrolNtetha <mphumier@gmail.com>
2023-09-16 15:14:14 +0200

    feat: moved files from pages to app dir

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Head from 'next/head';
 import React from 'react';
 import { Login } from '../../components/templates/login';
@@ -7,13 +9,13 @@ const LoginPage = () => {
         <React.Fragment>
           <Head>
             <title> Login Into Account </title>
-            <meta name='description' content='Login into your Tedious account.' />
+            <meta name='description' content='Login into your Duello account.' />
             <link rel='icon' href='/favicon.ico' />
           </Head>
 
-        <section className='bg-slate'>
-            <Login />
-        </section>
+            <section className='bg-slate'>
+                <Login />
+            </section>
         </React.Fragment>
     );
 };
