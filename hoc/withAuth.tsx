48dbc022211b4c100e320927b79ba8ad46048a46
ErrolNtetha <mphumier@gmail.com>
2023-09-29 14:36:41 +0200

    changed name

--- hoc/withAuth.tsx
+++ hoc/withAuth.tsx
@@ -1,27 +1,21 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { useAuth } from '../hooks/useAuth';
-import { useProfileStore } from '../hooks/useGlobalStore';
 
 export default function withAuth(Component: any) {
     return function ProtectedRoute({ ...props }) {
         const router = useRouter();
-        const { currentUser: isAuthenticated, loading } = useAuth();
-        const user = useProfileStore((state: any) => state?.profile);
-        console.log(user);
+        const { currentUser, loading } = useAuth();
 
         useEffect(() => {
             if (loading) {
                 console.log('loading');
             }
-            if (user && isAuthenticated && !user?.isApproved) {
-                router.push('/apply');
-            }
 
-            if (!user) {
+            if (!currentUser) {
                 router.push('/login');
             }
-        }, [user, loading, isAuthenticated, router]);
+        }, [loading, currentUser, router]);
 
         return <Component {...props} />;
     };
