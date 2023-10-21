d3e7388532710ab7b3484c09d2d4a80f8a065206
ErrolNtetha <mphumier@gmail.com>
2023-10-21 11:41:46 +0200

    feat: reduced picture size

--- components/organisms/vendor/profile/header.tsx
+++ components/organisms/vendor/profile/header.tsx
@@ -15,9 +15,9 @@ type IProps = {
 
 export default function ProfileHeader({ names, title, bio, avatar }: IProps) {
     return (
-        <section className=''>
+        <>
             <section className='flex md:flex-col md:justify-center gap-3'>
-                <section className='relative border border-gray rounded-full w-[60px] h-[60px] md:w-[80px] md:h-[80px]'>
+                <section className='relative border border-gray rounded-full w-[70px] h-[70px] md:w-[80px] md:h-[80px]'>
                     <Avatar className='w-full h-full'>
                         <AvatarImage src={avatar} alt='My avatar' />
                         <AvatarFallback>
@@ -39,6 +39,6 @@ export default function ProfileHeader({ names, title, bio, avatar }: IProps) {
             <section className='w-full mt-3'>
                 <p className='text-sm'> {bio} </p>
             </section>
-        </section>
-  )
-}
+        </>
+  );
+};
