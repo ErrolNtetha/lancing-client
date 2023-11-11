d607ed965f5785e8238f6aa3df41ea6fc62419ae
ErrolNtetha <mphumier@gmail.com>
2023-11-11 13:45:45 +0200

    feat: added a list

--- components/organisms/vendor/myList/lists.ts
+++ components/organisms/vendor/myList/lists.ts
@@ -6,6 +6,34 @@ export const lists = [
         },
         avatar: null,
         title: 'Logo Designer',
+        cover: '/assets/images/list.png',
+        category: 'Programming and software development',
+        isActive: true,
+        description: 'I will do wireframe, design and develop your website.',
+        ratings: {
+            reviews: [],
+            rating: 4,
+        },
+        packages: [
+            {
+                price: 399.99,
+                rate: 'hrly',
+                level: 'premium'
+            },
+            {
+                price: 89.99,
+                rate: 'hrly',
+                level: 'basic'
+            },
+        ],
+    },
+    {
+        names: {
+            firstName: 'Mphumeleli Errol',
+            lastName: 'Ntetha'
+        },
+        avatar: null,
+        title: 'Logo Designer',
         cover: '/assets/images/veges.jpg',
         category: 'Graphic Designing and Multimedia',
         isActive: false,
