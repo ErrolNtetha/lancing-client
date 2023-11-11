5dcc8f7ed33350d1726019ab27be40262eee98d6
ErrolNtetha <mphumier@gmail.com>
2023-11-11 11:37:07 +0200

    refactor: changed cover image

--- components/organisms/vendor/myList/lists.ts
+++ components/organisms/vendor/myList/lists.ts
@@ -6,7 +6,7 @@ export const lists = [
         },
         avatar: null,
         title: 'Logo Designer',
-        cover: '/assets/images/veges.jpg',
+        cover: '/assets/images/list.png',
         category: 'Graphic Designing and Multimedia',
         isActive: true,
         description: 'I will create custom logo for your company or personal brand.',
@@ -16,12 +16,12 @@ export const lists = [
         },
         packages: [
             {
-                price: 3499.99,
+                price: 349.99,
                 rate: 'hrly',
                 level: 'premium'
             },
             {
-                price: 1199.99,
+                price: 129.99,
                 rate: 'hrly',
                 level: 'basic'
             },
