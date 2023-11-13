ede7b5fc2867b92aba927c69eaaa7d442e0e2002
ErrolNtetha <mphumier@gmail.com>
2023-11-13 12:24:02 +0200

    feat: added a button to add list

--- components/templates/myList/index.tsx
+++ components/templates/myList/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Button } from '../../../@/components/ui/button';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../../../@/components/ui/dialog';
 import List from './list';
 
 export default function MyList() {
     return (
         <section className='m-3 md:container'>
-            {/* <h2 className='my-2 font-bold text-lg'> You have 1 listing. </h2> */}
             <List />
+
+            {/* Button to add list */}
+            <section className='fixed bottom-0 bg-background left-0 w-full p-2'>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button className='bg-primary font-bold w-full'>
+                            Create New List
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle> Add New List </DialogTitle>
+                        </DialogHeader>
+                        <section>
+                            Label
+                        </section>
+                        <DialogFooter>
+                            <Button type='submit'> Create </Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+            </section>
         </section>
     );
 };
