bb898936a592c98b7761f3fde756b5af8bb48e48
ErrolNtetha <mphumier@gmail.com>
2023-11-29 18:24:04 +0200

    feat: added a new image for meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ export default function RootLayout({
         <html>
             <head>
                 <title> Find Freelancers for Your Project | Tedcrunch </title>
-                <meta property='og:title' content='Find Freelancers for Your Project' />
+                <meta property='og:title' content='Find Freelancers in South Africa - Tedcrunch' />
                 <meta property='og:description' content='Tedcrunch is a South African freelance marketplace for businesses or individuals to find highly skilled freelancers.' />
                 <meta property='og:type' content='website' />
-                <meta property='og:image' content='/assets/images/list.png' />
+                <meta property='og:image' content='/assets/images/seo/logo.png' />
                 <meta property='og:url' content='https://www.tedcrunch.co.za' />
                 <meta property='twitter:card' content='summary_large_image' />
             </head>
