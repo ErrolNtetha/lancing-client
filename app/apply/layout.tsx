c9852f9831d2bdaefdb2bc761900421f6abba1d7
ErrolNtetha <mphumier@gmail.com>
2023-11-30 09:46:24 +0200

    feat: added more SEO meta tags

--- app/apply/layout.tsx
+++ app/apply/layout.tsx
@@ -10,7 +10,16 @@ export default function RootLayout({
 }) {
     return (
         <html>
-            <head></head>
+            <head>
+                <title> Apply | Tedcrunch </title>
+                <meta property='og:title' content='Create Application - Tedcrunch' />
+                <meta property='og:description' content='Create a new application and start applying for available jobs. It is quick and easy.' />
+                <meta property='og:type' content='website' />
+                <meta property='og:image' content='/assets/images/seo/logo.png' />
+                <meta property='og:url' content='https://www.tedcrunch.co.za' />
+                <meta property='twitter:card' content='summary_large_image' />
+                <meta property='og:site_name' content='Tedcrunch South Africa' />
+            </head>
             <body>
                 <main className='p-3'>
                 {children}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,6 +4,15 @@ import { Registration } from '../../components/templates/register';
 const Register = () => {
     return (
         <section className='bg-background h-[92vh]'>
+            <head>
+                <title> Register New Account | Tedcrunch </title>
+                <meta property='og:title' content='Register New Account. - Tedcrunch' />
+                <meta property='og:description' content='Create a new account in 15 seconds to find freelancers.' />
+                <meta property='og:type' content='website' />
+                <meta property='og:image' content='/assets/images/seo/logo.png' />
+                <meta property='og:url' content='https://www.tedcrunch.co.za' />
+                <meta property='twitter:card' content='summary_large_image' />
+            </head>
             <Registration />
         </section>
     );
