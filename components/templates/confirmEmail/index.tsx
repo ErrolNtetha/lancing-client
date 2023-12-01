dc83eb3f1c5691f2cc0470282ab5a6522fd4f306
ErrolNtetha <mphumier@gmail.com>
2023-12-01 15:49:51 +0200

    feat: added a success and error status when sending email

--- components/templates/confirmEmail/index.tsx
+++ components/templates/confirmEmail/index.tsx
@@ -2,9 +2,9 @@
 
 import { sendEmailVerification } from 'firebase/auth';
 import React from 'react';
-import { FcInvite } from 'react-icons/fc';
 import { Button } from '../../../@/components/ui/button';
 import { useAuth } from '../../../hooks/useAuth';
+import { MoonLoader } from 'react-spinners';
 
 /*
  * TO DO:
@@ -14,20 +14,23 @@ import { useAuth } from '../../../hooks/useAuth';
 
 export default function Email() {
     const { currentUser } = useAuth();
-    console.log(currentUser);
+    const [loading, setLoading] = React.useState(false);
+    const [isSuccessful, setIsSuccessful] = React.useState<null | boolean>(null);
+    const [isUnSuccessful, setIsUnSuccessful] = React.useState(false);
 
     const handleSendEmail = () => {
+        setLoading(true);
+        setIsUnSuccessful(false);
+
         sendEmailVerification(currentUser)
-            .then(() => console.log('Email sent. Check your inbox.'))
-            .catch((error) => console.log('An error occurred: ', error));
+            .then(() => setIsSuccessful(true))
+            .catch(() => setIsUnSuccessful(true))
+            .finally(() => setLoading(false));
     };
 
     return (
         <section className='md:container flex justify-center md:max-w-lg m-4 h-[92vh]'>
             <section className='text-sm text-center px-4 py-6'>
-                <span className=''>
-                <FcInvite className='text-6xl' />
-                </span>
                 <h1 className='text-3xl mt-3 text-center font-extrabold'> Almost there </h1>
                 <h3 className=''> Please verify your email address. </h3>
 
@@ -35,7 +38,18 @@ export default function Email() {
 
                 <span className='flex flex-col justify-center items-center w-full'>
                     <p className='block'> Didn&apos;t get the email? </p>
-                    <Button className='block font-semibold mt-2' onClick={handleSendEmail}> Resend Email </Button>
+                    <Button className='block px-4 mt-2' disabled={loading} onClick={handleSendEmail}>
+                        {loading ? 
+                            (
+                                <section className='flex items-center gap-3'>
+                                    Sending... 
+                                </section>
+                            )
+                            : 'Resend Email '
+                        }
+                    </Button>
+                    {isSuccessful && <p className='text-green-900 mt-4'> Email has been successfully sent. Check your emails. </p> }
+                    {isUnSuccessful && <p className='text-red-900 mt-4'> Something went wrong. Please try again later or contact us. </p>}
                 </span>
             </section>
         </section>
