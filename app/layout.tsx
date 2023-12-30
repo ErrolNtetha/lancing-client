d1e5d119d746876c9e713d89a9cf691bfb252fde
ErrolNtetha <mphumier@gmail.com>
2023-12-30 11:15:28 +0200

    feat: added Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import { Header } from '../components/organisms/header/header';
 import '../app/globals.css';
 import { Toaster } from '../@/components/ui/toaster';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
 export default function RootLayout({
     children,
@@ -8,7 +11,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html>
+        <html className={inter.className}>
             <head>
                 <title> Find Freelancers for Your Project | Tedcrunch </title>
                 <meta property='og:title' content='Find Freelancers in South Africa - Tedcrunch' />
