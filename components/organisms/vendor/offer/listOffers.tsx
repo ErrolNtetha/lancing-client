53c00aabf966f5efdd4c721c742ccfb396b9e277
ErrolNtetha <mphumier@gmail.com>
2024-01-07 14:33:44 +0200

    refactor: changed offers to be dynamic

--- components/organisms/vendor/offer/listOffers.tsx
+++ components/organisms/vendor/offer/listOffers.tsx
@@ -1,12 +1,33 @@
 'use client'
 
-import React from 'react'
+import { collection, doc, getDocs, query, where } from 'firebase/firestore';
+import React, { useState } from 'react'
 import { Separator } from '../../../../@/components/ui/separator';
+import { db } from '../../../../firebaseConfig';
+import { useAuth } from '../../../../hooks/useAuth';
 import OfferCard from './offerCard';
-import { offers } from './offers';
+// import { offers } from './offers';
 
 export default function ListOffersCards() {
-    const listOffers = offers.map((item) => <OfferCard key={item.id} {...item} />);
+    const { currentUser } = useAuth();
+    const [offers, setOffers] = useState<any>([]);
+
+    React.useEffect(() => {
+        const getOffers = async () => {
+            const offersRef = collection(db, 'offers');
+            const q = query(offersRef, where('to', '==', doc(db, `users/${currentUser?.uid}`)));
+
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data());
+                setOffers((prevState: any) => [...prevState, { id: doc.id, ...doc.data() }]);
+            });
+        };
+
+        getOffers();
+    }, [currentUser]);
+
+    const listOffers = offers.map((item: any) => <OfferCard key={item.id} {...item} />);
 
     return (
         <section className='flex-[0.3] md:border md:border-gray-200 md:rounded-md md:p-2'>
