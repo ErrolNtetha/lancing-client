ebd82cc24bbf448e22ca1ab862375cc07d26d38f
ErrolNtetha <mphumier@gmail.com>
2024-02-07 10:44:34 +0200

    refactor: changed route for milestones

---
+++ components/templates/offers/offerId/activity/add-milestones/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddMilestone from './add-milestones';
+
+export default function AddMilestones() {
+    return (
+        <div>
+            <AddMilestone />
+        </div>
+    );
+};
