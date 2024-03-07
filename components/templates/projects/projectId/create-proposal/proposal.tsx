d1a0f23429d622c04b398fe758a0f2c82efed102
ErrolNtetha <mphumier@gmail.com>
2024-03-07 22:55:43 +0200

    style: removed bg color

--- components/templates/projects/projectId/create-proposal/proposal.tsx
+++ components/templates/projects/projectId/create-proposal/proposal.tsx
@@ -79,6 +79,7 @@ export default function Proposal() {
             });
 
             toast({
+                className: '',
                 title: 'Success',
                 description: 'Proposal successfully sent.'
             });
