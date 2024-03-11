0101bc2a3957dfb6e02114b46661cb79ddf30798
ErrolNtetha <mphumier@gmail.com>
2024-03-11 08:45:02 +0200

    style: adjusted height

--- components/templates/myList/list.tsx
+++ components/templates/myList/list.tsx
@@ -58,10 +58,10 @@ export default function List() {
     ));
 
     const renderEmptyList = (
-        <section className='flex items-center justify-center h-[95vh]'>
+        <section className='flex items-center justify-center h-[92vh]'>
             <section className='flex items-center justify-center flex-col'>
-                <h2 className='font-bold text-center'> No lists added </h2>
-                <p className='max-w-lg text-center'> All your lists will appear here once they are added. </p>
+                <h2 className='font-bold text-center'> No lists added yet </h2>
+                <p className='max-w-md text-center'> All your lists will appear here once you add them. </p>
             </section>
         </section>
     )
